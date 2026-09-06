e452ec6e0cb69da2fc5058e5d540a0d08fd20f9b
hevesibank <h.gaborbank@gmail.com>
2026-09-06 15:42:45 +0200

    ver 4.2

--- engines/casino/app/controllers/casino/admin/base_controller.rb
+++
@@ -1,9 +0,0 @@
-module Casino
-  module Admin
-    class BaseController < Casino::ApplicationController
-      before_action :require_admin!
-      layout "casino/admin"
-    end
-  end
-end
-
--- engines/casino/app/models/casino/application_record.rb
+++
@@ -1,6 +0,0 @@
-module Casino
-  class ApplicationRecord < ::ApplicationRecord
-    self.abstract_class = true
-  end
-end
-
